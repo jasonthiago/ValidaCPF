3559515e52c7217f35d6ef5254fa5400507d229a
Jason Alencar <jasonthiago@gmail.com>
2015-12-22 17:13:04 -0200

    Mudança na mensagem de erro.

--- src/br/com/jason/cpf/view/Main.java
+++ src/br/com/jason/cpf/view/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		
 		if(cpf.validaCPF(cpf.getNumeroCPF()))
 			JOptionPane.showMessageDialog(null, "O CPF digitado foi "+ cpf.getNumeroCPF());
-		else JOptionPane.showMessageDialog(null, "Número de CPF Inválido!");
+		else JOptionPane.showMessageDialog(null, "Número de CPF Inválido.");
 		
 	}
 }
